d881b9882fd8ddb0ca6e9cc93d76e4165eaa9c5b
jcr678 <51676314+jcr678@users.noreply.github.com>
2021-03-28 17:18:24 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,12 @@
 import pickle
 import spacy
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.porter import *
+import pickle
+import spacy
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.porter import *
+import nltk
 def parse_name(nameString):
     nlp = spacy.load('en_core_web_md')
     doc = nlp(nameString)
@@ -11,10 +18,20 @@ def parse_name(nameString):
         parse_name(nameString)
 
 def return_topic_chose(topic_string, topics):
-    #do like a regex search
-    pass
-def ret_topic_bots(topics_to_doc):
-    return None
+    #stemmer = PorterStemmer()
+    wnl = WordNetLemmatizer()
+    topic_string_tokens =  [str(t).lower() for t in nltk.word_tokenize(topic_string)]
+    topic_string_lemma = [wnl.lemmatize(t)  for t in topic_string_tokens]
+    #do like a regex search but stemmed to take care of words that end with "s" or "ing"
+    topics_lemma = {wnl.lemmatize(t):t for t in topics}
+    #print(topics_lemma)
+    for lem in list(topics_lemma.keys()):
+        if lem in topic_string_lemma:
+            return topics_lemma[lem]
+    topic_string = input("Sorry I didnt get that can you repeat that?")
+    return return_topic_chose(topic_string, topics)
+def ret_topic_bots(topic_to_doc):
+    return "None"
 
 def main():
     nameString = input("Hello, welcome to Austin. What is your name?")
@@ -25,9 +42,9 @@ def main():
     #get name using named entity recognition
     print("I can talk about " + ", ".join(topics[:len(topics)-1]) + ", and " + topics[-1])
     topic_string = input("What topic do you want to talk about?")
-    topic_chosen = return_topic_chose(topic_string)
-    topic_bots = ret_topic_bots(topics_to_doc)
-    print("Sounds good, when you want to talk about another one of those topics please let me know")
+    topic_chosen = return_topic_chose(topic_string, topics)
+    topic_bots = ret_topic_bots(topic_to_doc)
+    print("Sounds good, you want to talk about " + topic_chosen +". when you want to talk about another one of those topics please let me know")
     bot_dict = {topic: None for topic in topics} #put bots here
     while True:
         #let bot on topic talk
